fd3412c711158b66483b4ec3d281704a646f7ddb
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-30 21:27:20 +0700

    Initial Commit

---
+++ backend/src/main/java/com/swan/aistudio/AistudioApplication.java
@@ -0,0 +1,13 @@
+package com.swan.aistudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AistudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AistudioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/swan/aistudio/AistudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swan.aistudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AistudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
